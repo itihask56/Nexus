21890ee545b4a818f913028bb1b010cf01cdf39e
Itihas Verma <itihask56@gmail.com>
2024-12-20 14:58:20 +0530

    javascript day 17 done

--- Javascript/Day17/index.js
+++ Javascript/Day17/index.js
@@ -1,31 +1,20 @@
-
-// The this keyword in JavaScript is a special keyword that refers to 
-// the context in which the current code is being executed. 
+// The this keyword in JavaScript is a special keyword that refers to
+// the context in which the current code is being executed.
 // Its value depends on how the function where this is used is called.
 
-
 // 1: Global Context (Outside Any Function)
-// In browsers: window 
-// In Node.js: Module's exports object 
-
+// In browsers: window
+// In Node.js: Module's exports object
 
 // console.log(this);
 
-
-
-
-
-
 // ****************************************************************
 
-
-// 2:Inside a Function 
+// 2:Inside a Function
 // i: (Non-Strict Mode)
 // When this is used inside a regular function, it refers to the global object.
 // ii: Strict Mode
 // this will be undefined inside a function.
-
-
 
 // function greet(){
 //     console.log(this);
@@ -49,46 +38,34 @@
 
 // obj.meet();
 
-
-
-
 // ****************************************************************
 
-// Arrow functions don’t have their own this. 
+// Arrow functions don’t have their own this.
 // Instead, they inherit this from the surrounding (lexical) scope.
 
 // let obj = {
-//     name:"rohit",
-//     age:11,
-//     greet: ()=>{
-//         console.log(this);
-//     }
-// }
+//   name: "rohit",
+//   age: 11,
+//   greet: () => {
+//     console.log(this);
+//   },
+// };
 
 // obj.greet();
 
+let obj = {
+  name: "rohit",
+  age: 11,
+  greet: function () {
+    let ab = () => {
+      console.log(this);
+    };
 
-// let obj = {
-//     name:"rohit",
-//     age:11,
-//     greet: function(){
-        
-//         let ab = ()=>{
-//             console.log(this);
-//         };
+    ab();
+  },
+};
 
-//         ab();
-
-//    }
-// }
-
-
-
-// obj.greet();
-
-
-
-
+obj.greet();
 
 // Inside a Constructor or Class
 // In constructors and classes, this refers to the instance of the object being created.
@@ -100,12 +77,8 @@
 //     }
 // }
 
-
-
 // let a = new Person("Rohit", 20);
 // console.log(a);
-
-
 
 // let greet = ()=>{
 //     console.log(this);
@@ -113,14 +86,10 @@
 
 // greet();
 
-"use strict"
+("use strict");
 
-let meet = function(){
-    console.log(this);
-}
+let meet = function () {
+  console.log(this);
+};
 
 meet();
-
-
-
-
